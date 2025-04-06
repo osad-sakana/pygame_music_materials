6a0596f1e27f55ae0708372c185ba1c52c656ebd
osad-sakana <mezzoforte.mackerel@icloud.com>
2025-04-06 13:59:31 +0900

    MANIFEST.inを追加し、setup.pyでバージョンを0.1.1に更新。パッケージデータに音楽ファイルを含める設定を追加。

--- setup.py
+++ setup.py
@@ -2,8 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="pygame_music_materials",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        "pygame_music_materials": ["musics/*.mp3"],
+    },
     install_requires=[
         "pygame>=2.5.2",
     ],
